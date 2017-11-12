11cca8280a1f7677f6f71e1552fbd821b7247da3
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-11-13 02:15:54 +0300

    Completed

--- src/CardForm.js
+++ src/CardForm.js
@@ -26,12 +26,37 @@ export class CardForm extends Component {
     clearInterval(this.id);
   }
 
+  handleChangeForm = e => {
+    const {onChangeForm} = this.props;
+    onChangeForm(e.target.name, e.target.value.replace(/\s/g, '').substring(0, 16));
+  }
+
+  pretifyCardNumber = () => {
+    let cardNumber = '';
+    // Чтобы не падали тесты
+    if (this.props.cardNumber) {
+      let cardNumber = this.props.cardNumber.replace(/(.{4})/g, '$1 ');
+      if (cardNumber.lastIndexOf(' ') === cardNumber.length - 1) cardNumber = cardNumber.substring(0, cardNumber.length - 1);
+    }
+    return cardNumber;
+  }
+
   render() {
     const {leftTime} = this.state;
+
     return (
-      <div>
+      <div className="CardForm card-form">
         <Title>Номер карты</Title>
-        <p className="left-time">Осталось {leftTime} секунд</p>
+        <p className="CardForm__left-time left-time">Осталось {leftTime} секунд</p>
+        <div className="CardForm__field">
+          <input 
+            className="App__textfield"
+            placeholder="0000-0000-0000-0000"
+            name="cardNumber"
+            onChange={this.handleChangeForm}
+            value={this.pretifyCardNumber()}
+          />
+        </div>
       </div>
     );
   }
--- src/PersonalForm.js
+++ src/PersonalForm.js
@@ -3,10 +3,44 @@ import Title from './Title';
 import './PersonalForm.css';
 
 export class PersonalForm extends Component {
+  handleChangeForm = e => {
+    const {onChangeForm} = this.props;
+    onChangeForm(e.target.name, e.target.value);
+  }
+
   render() {
+    const {firstName, lastName, email} = this.props;
+
     return (
-      <div>
+      <div className="PersonalForm personal-form">
         <Title>Персональная информация</Title>
+        <div className="PersonalForm__field">
+          <input 
+            className="App__textfield"
+            placeholder="Имя"
+            name="firstName"
+            onChange={this.handleChangeForm}
+            value={firstName}
+          />
+        </div>
+        <div className="PersonalForm__field">
+          <input 
+            className="App__textfield"
+            placeholder="Фамилия"
+            name="lastName"
+            onChange={this.handleChangeForm}
+            value={lastName}
+          />
+        </div>
+        <div className="PersonalForm__field">
+          <input 
+            className="App__textfield"
+            placeholder="E-mail"
+            name="email"
+            onChange={this.handleChangeForm}
+            value={email}
+          />
+        </div>
       </div>
     );
   }
